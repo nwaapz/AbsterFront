af5536225ed183cd69f613d69255f7cc2e135568
hosein <jexcoht@gmail.com>
2025-08-25 14:56:03 +0330

    wagmi connected to abstract

--- my-agw-app/src/Providers.tsx
+++ my-agw-app/src/Providers.tsx
@@ -1,12 +1,12 @@
 // ./src/Providers.tsx
 import { AbstractPrivyProvider } from "@abstract-foundation/agw-react/privy";
-import { abstract } from "viem/chains"; // Import your chain here
+import { abstract, abstractTestnet, sepolia } from "viem/chains"; // Import your chain here
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AbstractPrivyProvider
       appId="cmeq059tl021wl10b1s3v3v23" 
-      chain={abstract} // Required prop added
+      chain={abstractTestnet} // Required prop added
     >
       {children}
     </AbstractPrivyProvider>
